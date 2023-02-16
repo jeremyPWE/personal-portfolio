12405b7fea5d8378bfac35aa509b852a48b3b42a
jeremyPWE <jpwijanto@gmail.com>
2023-02-16 18:09:43 +0700

    add margin bottom to hero section

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ import Section from "./Section";
 function Hero() {
   return (
     <Section>
-      <div className="w-full flex items-center justify-center h-[80vh] lg:h-[75vh] lg:mb-[15vh]">
+      <div className="w-full flex items-center justify-center h-[80vh] lg:h-[75vh] mb-[10vh] lg:mb-[15vh]">
         <HeroAnimation />
       </div>
     </Section>
