c2a07c660481ffb0ebc0457ec85d92762d429d7e
jeremyPWE <jpwijanto@gmail.com>
2023-03-20 10:52:29 +0700

    set layout for leetcode75

--- pages/project/leetcode/index.js
+++ pages/project/leetcode/index.js
@@ -5,7 +5,16 @@ export default function Leetcode() {
   return (
     <>
       <Header />
-      <Layout></Layout>
+      <Layout>
+        <div className="px-[20px] flex gap-[20px] w-full max-w-[1920px] pt-[50px]">
+          <div className="w-1/5 block border border-red-200">
+            leetcode question
+          </div>
+          <div className="grow block border border-blue-400">
+            leetcode answer
+          </div>
+        </div>
+      </Layout>
     </>
   );
 }
