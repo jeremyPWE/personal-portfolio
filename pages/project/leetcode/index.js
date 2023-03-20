11a2dba67fe85dca05980ae5a1e26d4da33551ee
jeremyPWE <jpwijanto@gmail.com>
2023-03-20 09:33:58 +0700

    create new page for leetcode75

---
+++ pages/project/leetcode/index.js
@@ -0,0 +1,11 @@
+import Header from "../../../components/Header";
+import Layout from "../../../components/Layout";
+
+export default function Leetcode() {
+  return (
+    <>
+      <Header />
+      <Layout></Layout>
+    </>
+  );
+}
