8819b623c4648510ae46e8538917306c43818b58
jeremyPWE <jpwijanto@gmail.com>
2023-03-31 14:01:00 +0700

    add animation, refactor code

---
+++ components/Work.js
@@ -0,0 +1,32 @@
+import Section from "./Section";
+
+export const workTitle = [
+  {
+    title: "Merokket.id",
+    description: "Web Developer Freelance",
+    linkTo: "https://merokket.id",
+    period: "March 2021 - Present",
+    details: [
+      "Develop modern, high-performance, and maintainable code for both in-house and client websites, using a variety of languages, platforms, frameworks, and content management systems such as JavaScript, React, Next, WordPress, and Shopify.",
+      "Ensure cross-browser compatibility and responsiveness of sites by conducting manual testing on various browsers and mobile devices.",
+      "Serve as the primary point of contact between hosting companies, clients, and vendors, handling all verbal and written communications to ensure a smooth and efficient project process.",
+      "Meet with prospective clients to review existing websites or gather specifications for new websites, ensuring that their needs and goals are understood and incorporated into the design and development process.",
+      "Prepare multiple designs and wireframes for client approval before proceeding with development, ensuring that the end product aligns with their vision and requirements.",
+    ],
+  },
+  {
+    title: "Saweria",
+    description: "Frontend Developer Internship",
+    linkTo: "https://saweria.co",
+    period: "June 2022 - August 2022",
+    details: [
+      "Co-developed new streaming overlay feature that allows users to spin a wheel of prizes after reaching a certain donation amount. Increasing streamer-viewer engagement and donation tendencies.",
+      "Created a table feature for transaction tracking, improving usability and functionality.",
+      "Updated cash out page to offer eWallet or bank transfer options for greater flexibility.",
+      "Streamlined the donation process by limiting maximum character count for notifications.",
+      "Enhanced the FAQ section with accordion-style navigation for easier use.",
+      "Improved UI validations for minimum donation page.",
+      "Added bank name information to transaction list for improved tracking and organization.",
+    ],
+  },
+];
